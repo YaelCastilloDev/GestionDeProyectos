d13f596f928cc59ac9ded6b64e5cc4ac8e114793
Yael <yael.castillo.dev@gmail.com>
2024-11-21 19:04:19 -0600

    prueba

--- src/main/java/models/Proyecto.java
+++ src/main/java/models/Proyecto.java
@@ -8,15 +8,7 @@ public class Proyecto {
     @Size(max = 100, message = "El nombre del proyecto no debe exceder los 100 caracteres")
     private String Nombre;
 
-    @NotNull(message = "La fecha límite no puede estar vacía")
-    @Future(message = "La fecha límite debe ser una fecha futura")
-    private Date FechaLimite;
-
-    @NotNull(message = "La fecha de inicio no puede estar vacía")
-    @PastOrPresent(message = "La fecha de inicio debe ser hoy o en el pasado")
-    private Date FechaInicio;
-
-    @NotNull(message = "La fecha de entrega no puede estar vacía")
-    @PastOrPresent(message = "La fecha de entrega debe ser hoy o en el pasado")
-    private Date FechaEntregado;
+    @NotBlank(message = "Se debe anadir una descripcion")
+    @Size( max = 1000, message = "El nombre del proyecto no debe exceder los 1000 caracteres")
+    private String Descripcion;
 }
